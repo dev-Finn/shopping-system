e5bc0e97295a3e1962015b6e84c70dcd1a3995ce
Finn Dührkop <finn1999.duehrkop@gmail.com>
2023-01-03 22:05:51 +0100

    Payment MVP Done

---
+++ source/Payment/Payment.Components/Consumers/ProcessPaymentHandler.cs
@@ -0,0 +1,13 @@
+﻿using MassTransit;
+using Ordering.Contracts.Commands;
+using Payment.Contracts.Events;
+
+namespace Payment.Components.Consumers;
+
+public sealed class ProcessPaymentHandler : IConsumer<ProcessPayment>
+{
+    public async Task Consume(ConsumeContext<ProcessPayment> context)
+    {
+        await context.Publish(new PaymentProcessed(context.Message.OrderId), context.CancellationToken);
+    }
+}
